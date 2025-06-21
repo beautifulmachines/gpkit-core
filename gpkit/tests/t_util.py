78b31a940ac12f361810b634f56398ef8d0abbd6
whoburg <whoburg@alum.mit.edu>
2025-06-21 11:30:50 -0500

    unused import

--- gpkit/tests/t_util.py
+++ gpkit/tests/t_util.py
@@ -4,7 +4,7 @@ import unittest
 
 import numpy as np
 
-from gpkit import Model, NomialArray, Variable, VectorVariable, parse_variables
+from gpkit import Model, NomialArray, Variable, parse_variables
 
 
 class OnlyVectorParse(Model):
