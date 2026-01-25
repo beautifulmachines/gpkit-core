23fd5fe8a9bb09468820510fa1adbb50b5d70f20
Warren Hoburg <whoburg@alum.mit.edu>
2026-01-25 15:08:15 -0600

    move test helpers to conftest

--- gpkit/tests/conftest.py
+++ gpkit/tests/conftest.py
@@ -8,7 +8,55 @@ import pytest
 
 import gpkit
 from gpkit import settings
-from gpkit.tests.helpers import NewDefaultSolver, StdoutCaptured
+
+
+class NullFile:
+    "A fake file interface that does nothing"
+
+    def write(self, string):
+        "Do not write, do not pass go."
+
+    def close(self):
+        "Having not written, cease."
+
+
+class NewDefaultSolver:
+    "Creates an environment with a different default solver"
+
+    def __init__(self, solver):
+        self.solver = solver
+        self.prev_default_solver = None
+
+    def __enter__(self):
+        "Change default solver."
+        self.prev_default_solver = gpkit.settings["default_solver"]
+        gpkit.settings["default_solver"] = self.solver
+
+    def __exit__(self, *args):
+        "Reset default solver."
+        gpkit.settings["default_solver"] = self.prev_default_solver
+
+
+class StdoutCaptured:
+    "Puts everything that would have printed to stdout in a log file instead"
+
+    def __init__(self, logfilepath=None):
+        self.logfilepath = logfilepath
+        self.original_stdout = None
+
+    def __enter__(self):
+        "Capture stdout"
+        self.original_stdout = sys.stdout
+        sys.stdout = (
+            open(self.logfilepath, mode="w", encoding="utf-8")
+            if self.logfilepath
+            else NullFile()
+        )
+
+    def __exit__(self, *args):
+        "Return stdout"
+        sys.stdout.close()
+        sys.stdout = self.original_stdout
 
 
 @pytest.fixture(name="solver", params=settings["installed_solvers"])
