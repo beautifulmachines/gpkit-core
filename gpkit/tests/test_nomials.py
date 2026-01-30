50a04d7268eb2ad5dce7d7269e3d5242192eebd3
Warren Hoburg <whoburg@alum.mit.edu>
2026-01-30 07:46:45 -0600

    add failing test for issue 43

--- gpkit/tests/test_nomials.py
+++ gpkit/tests/test_nomials.py
@@ -85,6 +85,15 @@ class TestMonomial:  # pylint: disable=unnecessary-negation,comparison-with-itse
         if sys.platform[:3] != "win":
             assert r == "gpkit.Monomial(5·x²/y)"
 
+    def test_pow_ast_exponent(self):
+        "Signomial.__pow__ should record the correct exponent in the AST"
+        x = Variable("x")
+        p = x**3 + x + 5
+        p2_str = str(p**2)
+        assert "^0" not in p2_str
+        assert "^2" in p2_str
+        assert p2_str == "gpkit.Posynomial((x³ + x + 5)^2)"
+
     def test_latex(self):
         "Test latex string creation"
         x = Variable("x")
