213be05a5f85397b5c70d6d9450a8e1929c0a38d
Warren Hoburg <whoburg@alum.mit.edu>
2026-03-25 14:47:08 -0500

    add toml save_subs, load_subs, and apply_subs methods

--- gpkit/toml/__init__.py
+++ gpkit/toml/__init__.py
@@ -10,12 +10,16 @@ from ._parser import (
     load_toml,
 )
 from ._printer import to_toml
+from ._subs import apply_subs, load_subs, save_subs
 
 __all__ = [
     "TomlExpressionError",
     "TomlParseError",
+    "apply_subs",
+    "load_subs",
     "load_toml",
     "parse_constraint",
     "parse_objective",
+    "save_subs",
     "to_toml",
 ]
